9adc33b6fef9cf0d4b95f6662836cf23d53ea9c0
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-02-01 11:09:26 +0530

    feat: initialize backend with Express and MongoDB setup
    - Added package.json for backend dependencies and scripts
    - Created server.js to set up the Express server
    - Implemented app.js for handling routes and note operations
    - Established database connection in database.js
    - Defined note model in note-model.js

---
+++ day9/backend/src/app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const noteModel = require("./models/note-model")
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome to day-9 topics")
+})
+
+app.post("/api/notes", async (req,res)=>{
+    const {title, description} = req.body
+
+    const note = await noteModel.create({
+        title,
+        description
+    })
+
+    res.status(201).json({
+        message: "note created successfully.",
+        note
+    })
+})
+
+app.get("/api/notes", async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "Notes fetched successfully.",
+        notes
+    })
+})
+
+module.exports = app
