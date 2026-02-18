ce2b89dfbaf3d854e465432657f6089c6b344813
Bhavesh Kumar <bhaveshaadra26@gmail.com>
2026-02-18 15:30:54 +0530

    feat: add controllers for fetching posts and post details with token validation

--- day14/src/controllers/post.controller.js
+++ day14/src/controllers/post.controller.js
@@ -48,6 +48,103 @@ const createPostController = async (req,res)=>{
     })
 }
 
+const getPostController = async (req,res)=>{
+    
+    let token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message: "Invalid token or token required!"
+        })
+    }
+
+    let decoded = null;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch(err){
+        return res.status(401).json({
+            message: "user not authorized."
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    if(!posts){
+        return res.status(404).json({
+            message: "post not found."
+        })
+    }
+
+    if(posts.length === 0){
+        return res.status(404).json({
+            message: "There is no post uploaded yet."
+        })
+    }
+
+    res.status(200).json({
+        message: "posts fetched successfully.",
+        posts
+    })
+}
+
+const getPostDetailsController = async (req,res)=>{
+    try{
+        const token = req.cookies.token
+
+        if(!token){
+            return res.status(401).json({
+                message: "Invalid token or token required."
+            })
+        }
+
+        let decoded = null;
+
+        try{
+            decoded = jwt.verify(token, process.env.JWT_SECRET)
+        } catch(err){
+            return res.status(401).json({
+                message: "unauhorized access!"
+            })
+        }
+
+        const userId = decoded.id
+        const postId = req.params.postId
+
+        const post = await postModel.findById(postId)
+
+        if(!post){
+            return res.status(404).json({
+                message: "post not found."
+            })
+        }
+
+        const isValidUser = post.user.toString() === userId
+
+        if(!isValidUser){
+            return res.status(403).json({
+                message: "Forbidden Content"
+            })
+        }
+
+        res.status(200).json({
+            message: "Post fetched successfully.",
+            post
+        })
+
+    } catch(err){
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+}
+
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
